4a40954f56ce8bfc59dfb1d59e8445c07f0e6180
Serhat Polat <132306641+polatserhat@users.noreply.github.com>
2024-07-31 17:49:11 +0300

    first commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Serhat  on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
