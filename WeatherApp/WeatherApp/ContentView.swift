3847f2c38e9c774079780e0510fd48a41056ceea
Serhat Polat <132306641+polatserhat@users.noreply.github.com>
2024-08-01 17:22:16 +0300

    ContentView updated

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -9,11 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack{
+                Text("Istanbul,TR")
+                    .font(.system(size: 32,weight: .medium,design: .default))
+                    .foregroundStyle(.white)
+                    .padding()
+                
+               
+                VStack(spacing: 8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:180, height: 180)
+                    Text("32°")
+                        .font(.system(size: 70,weight: .medium,design: .default))
+                        .foregroundStyle(.white)
+                }
+                
+                VStack(spacing:2){
+                    HStack{
+                        VStack(spacing:4){
+                            Text("TUE")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                            Image(systemName: "cloud.sun.fill")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:50, height: 50)
+                            }
+                        VStack{
+                            Text("THU")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                            Image(systemName: "cloud.sun.fill")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:50, height: 50)
+                        }
+                        Text("FRI")
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                        Text("SAT")
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                    }
+                    
+                    
+                }
+                Spacer()
+            }
+            
         }
         .padding()
     }
